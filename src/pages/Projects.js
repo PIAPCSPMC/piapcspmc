9f3df232c8e5e8d6a894e679ae15b97c9e413e48
PIAPCSPMC <Kassymovlaw@gmail.com>
2025-02-13 12:01:48 +0500

    Initial commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,10 @@
 import React from 'react';
-
-import projectControlsImg from '../assets/images/aboutbanner.png';
-import deliverablesImg from '../assets/images/banner.png';
-import dashboardsImg from '../assets/images/contact.jpg';
-import ProjectControlsTable from './ProjectControlsTable';
 import ProjectControlsDeliverables from './ProjectControlsDeliverables';
 
 export default function Projects() {
   return (
     <section className="projects-page">
       <div className="container">
-        <ProjectControlsTable/>
         <ProjectControlsDeliverables/>
         {/* Project Controls System */}
         <div className="project-section">
